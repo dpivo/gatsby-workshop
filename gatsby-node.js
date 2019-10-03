3ab0081d451c573e9cb77e2b284f92d712d80306
dylanpivo <dpivo@greenpeace.org>
2019-10-03 14:49:09 +0200

    lets get this live

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`)
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date] }) {
+        edges {
+          node {
+            wordpress_id
+            title
+            excerpt
+            content
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allWordpressPost.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="home" />
+      <h1>My WordPress Blog</h1>
+      <h4>Posts</h4>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div>
+          <Link to={`/${node.slug}`}>
+            <p>{node.title}</p>
+          </Link>
+          {node.featured_media && node.featured_media.localFile.childImageSharp && <img src={node.featured_media.localFile.childImageSharp.fluid.src} alt={node.title} title={node.title} />}
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      ))}
+    </Layout>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allWordpressPost {
+      edges {
+        node {
+          wordpress_id
+          title
+          excerpt
+          slug
+          featured_media {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 1024) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
